ecc04cac9f22143456ce427998611daa896db899
grobelDev <56764278+grobelDev@users.noreply.github.com>
2020-03-09 13:47:39 -0700

    minor formatting

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {screenshot} = require('./screenshot.js')
+const { screenshot } = require('./screenshot.js');
 
 const express = require('express');
 const puppeteer = require('puppeteer');
@@ -9,9 +9,9 @@ const app = express();
 app.use(screenshot);
 
 const server = app.listen(process.env.PORT || 8080, err => {
-    if (err) return console.error(err);
-    const port = server.address().port;
-    console.info(`App listening on port ${port}`);
-  });
+  if (err) return console.error(err);
+  const port = server.address().port;
+  console.info(`App listening on port ${port}`);
+});
 
 module.exports = app;
